37d28c28e5de2aec4d4ceffafc6f9e037a4740b7
Oumar Diane <oumardiane399@gmail.com>
2025-08-27 14:50:15 +0200

    first commit1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://g-traf.vercel.app",
+      "https://jess-voyage.vercel.app",
       "https://admingtraf.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
